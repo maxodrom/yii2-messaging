be9b06c3173abe71cc9e573cc6d1a3190b969b88
Max Alexandrov <max@7u3.ru>
2018-05-29 06:40:44 +0300

    Translation messages files.

---
+++ src/messages/ru/common.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Yii2 messaging module.
+ *
+ * @author Max Alexandrov <max@7u3.ru>
+ * @copyright Copyright (c) Max Alexandrov, 2018
+ * @license MIT License
+ */
+
+return [
+    'Messaging' => 'Обмен сообщениями',
+];
