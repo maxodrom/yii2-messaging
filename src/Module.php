a42ae51b4902f8bce5deab4e15c27130fd0214c7
Max Alexandrov <max@7u3.ru>
2018-05-29 04:33:56 +0300

    Extend module definition.

--- src/Module.php
+++ src/Module.php
@@ -9,11 +9,63 @@
 
 namespace maxodrom\messaging;
 
+use Yii;
+use yii\base\BootstrapInterface;
+use yii\console\Application as ConsoleApp;
+use yii\db\Connection;
+use yii\di\Instance;
+use yii\web\Application as WebApp;
+
 /**
  * Class Module
  * @package maxodrom\messaging
+ * @since 1.0
  */
-class Module extends \yii\base\Module
+class Module extends \yii\base\Module implements BootstrapInterface
 {
+    public $controllerNamespace = 'maxodrom\messaging\controllers';
+    /**
+     * @var \yii\db\Connection|array|string DB component.
+     */
+    public $db = 'db';
 
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        $this->db = Instance::ensure($this->db, Connection::class);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function bootstrap($app)
+    {
+        $this->setAliases([
+            '@messaging' => '@vendor/maxodrom/yii2-messaging/src',
+        ]);
+
+        if ($app instanceof WebApp) {
+            // do nothing
+        } elseif ($app instanceof ConsoleApp) {
+            $this->controllerNamespace = 'maxodrom\messaging\commands';
+            Yii::setAlias('@maxodrom/messaging/commands', __DIR__ . '/commands');
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function beforeAction($action)
+    {
+        if (!parent::beforeAction($action)) {
+            return false;
+        }
+        $this->db->tablePrefix = $this->id . '_';
+
+        return true;
+    }
 }
