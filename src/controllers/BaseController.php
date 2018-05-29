0ecb330276085bd7015ec1cebaf9655dabadaeed
Max Alexandrov <max@7u3.ru>
2018-05-29 04:27:01 +0300

    Base controller.

---
+++ src/controllers/BaseController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Yii2 messaging module.
+ *
+ * @author Max Alexandrov <max@7u3.ru>
+ * @copyright Copyright (c) Max Alexandrov, 2018
+ * @license MIT License
+ */
+
+namespace maxodrom\messaging\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Class BaseController
+ * @package maxodrom\messaging\controllers
+ * @since 1.0
+ */
+abstract class BaseController extends Controller
+{
+
+}
