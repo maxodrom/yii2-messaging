6dcf4fc9d9fa5a86a472370a274c4c1e24c6dcc1
Max Alexandrov <max@7u3.ru>
2018-05-29 04:20:03 +0300

    Base module class.

---
+++ src/Module.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Yii2 messaging module.
+ *
+ * @author Max Alexandrov <max@7u3.ru>
+ * @copyright Copyright (c) Max Alexandrov, 2018
+ * @license MIT License
+ */
+
+namespace maxodrom\messaging;
+
+/**
+ * Class Module
+ * @package maxodrom\messaging
+ */
+class Module extends \yii\base\Module
+{
+
+}
